d9289f2539e2d577172e913247755a80b0964f52
Marlon Marcello <marlon.marcello@gmail.com>
2024-05-07 22:00:16 -0700

    fixed nvim init

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,9 +1,5 @@
-vim.cmd("number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set autoindent")
-vim.cmd("set mouse=a")
-vim.cmd("set relativenumber")
-
