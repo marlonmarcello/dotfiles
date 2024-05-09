e79c1e39f8f6e25153bf7180995e1629ae08b97d
Marlon Marcello <marlon.marcello@gmail.com>
2024-05-08 22:49:46 -0700

    theme definitions

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,6 +6,8 @@ fish_config theme choose "DraculaOfficial"
 
 set fzf_fd_opts --hidden --max-depth 5 -E .git
 
+fish_ssh_agent
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
   eval (zellij setup --generate-auto-start fish | string collect)
--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -16,6 +16,8 @@ config.font = wezterm.font_with_fallback {
 }
 config.font_size = 16
 config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
 
 -- and finally, return the configuration to wezterm
 return config
