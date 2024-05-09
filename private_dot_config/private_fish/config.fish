5e23b965b251788fd126a2582d09cf52650b1b71
Marlon Marcello <marlon.marcello@gmail.com>
2024-05-08 22:21:16 -0700

    fix editor

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,8 +1,14 @@
+set -gx EDITOR nvim
+
+set -gx VISUAL code --wait
+
+fish_config theme choose "DraculaOfficial"
+
+set fzf_fd_opts --hidden --max-depth 5 -E .git
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
   eval (zellij setup --generate-auto-start fish | string collect)
 end
 
-fish_config theme choose "DraculaOfficial"
 
-set fzf_fd_opts --hidden --max-depth 5 -E .git
